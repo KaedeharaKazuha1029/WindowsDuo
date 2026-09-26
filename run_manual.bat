d69cc4c2d4a7a7884ece54faca8aafc67d0991fb
L-web666 <3659874078@qq.com>
2026-09-26 16:57:35 +0800

    Delete run_manual.bat

--- run_manual.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Manual mode: no ESP needed. Up/Down = glass +/-  Right = 100%  Left = clear  Esc = quit.
-cd /d "%~dp0"
-where py >nul 2>nul && (set "PY=py -3.14") || (set "PY=python")
-%PY% -u glass_overlay.py --manual %*
-pause
